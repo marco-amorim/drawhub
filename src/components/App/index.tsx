45932395fb7bb68484e5ffb7ba8bae99efebad81
marco-amorim <marcoamorim95@hotmail.com>
2021-01-14 08:45:09 -0300

    removed auto generated files

---
+++ src/components/App/index.tsx
@@ -0,0 +1,7 @@
+import Header from '../Header';
+
+const App = () => {
+	return <Header />;
+};
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return <header>Header</header>;
+	}
+}
+
+export default Header;
