6cdbda3f74dd50c0352842bec35b845679cbed9d
marco-amorim <marcoamorim95@hotmail.com>
2021-01-19 15:28:19 -0300

    fixed bug on component life cycle and added key on comments map function

--- src/components/CommentsList/index.tsx
+++ src/components/CommentsList/index.tsx
@@ -24,35 +24,37 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const CommentsList: React.FC<CommentsListProps> = ({ docId }) => {
-	useEffect(() => {
-		async function fetchComments() {
-			await renderComments();
-		}
-
-		fetchComments();
-	});
-
 	const classes = useStyles();
 	const [comments, setComments] = useState([]);
+
+	useEffect(() => {
+		let isMounted = true;
+
+		renderComments().then((comments) => {
+			if (isMounted) setComments(comments);
+		});
+
+		return () => {
+			isMounted = false;
+		};
+	});
 
 	async function renderComments() {
 		try {
 			const commentsArray = await getComments(docId);
-			setComments(
-				commentsArray.map((comment: any, index: number) => {
-					return (
-						<>
-							<Comment
-								text={comment.text}
-								createdAt={comment.createdAt}
-								displayName={comment.displayName}
-								photoURL={comment.photoURL}
-							/>
-							<Divider variant="inset" component="li" />
-						</>
-					);
-				})
-			);
+			return commentsArray.map((comment: any, index: number) => {
+				return (
+					<React.Fragment key={index}>
+						<Comment
+							text={comment.text}
+							createdAt={comment.createdAt}
+							displayName={comment.displayName}
+							photoURL={comment.photoURL}
+						/>
+						<Divider variant="inset" component="li" />
+					</React.Fragment>
+				);
+			});
 		} catch (error) {
 			console.log(
 				'We are having trouble trying to fetch the comments, error: ' + error
